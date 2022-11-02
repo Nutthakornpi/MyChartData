5248910c398ed73b378d4df15e90bc499bad7835
nutthakornpi <s64309010008@ptnicec.ac.th>
2022-11-02 10:52:13 +0700

    ส่งครั้งที่ 1

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,6 +6,49 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  
+  public barChartOptions:any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels:string[] = ['2006', '2007', '2008', '2009'];
+  public barChartType:string = 'bar';
+  public barChartLegend:boolean = true;
+  
+  public barChartData:any[] = [
+    {data: [65, 59, 80, 81], label: 'จำนวนเเรกเข้า'},
+    {data: [28, 48, 40, 19], label: 'จำนวนสำเร็จการศึกษา'}
+  ];
+  
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+  
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
+  
+  public randomize():void {
+    // Only Change 3 values
+    let data = [
+      Math.round(Math.random() * 100),
+      59,
+      80,
+      (Math.random() * 100),
+      56,
+      (Math.random() * 100),
+      40];
+    let clone = JSON.parse(JSON.stringify(this.barChartData));
+    clone[0].data = data;
+    this.barChartData = clone;
+    /**
+     * (My guess), for Angular to recognize the change in the dataset
+     * it has to change the dataset variable directly,
+     * so one way around it, is to clone the data, change it and then
+     * assign it;
+     */
+  }
 
   constructor(public navCtrl: NavController) {
 
